17a327a314cda2458b21102fb1861a81f2939492
Lucas Frederico <lucas.frederico@outlook.com.br>
2026-05-28 23:48:31 -0300

    chore: scaffold Spring Boot 4 / Java 25 project

---
+++ src/main/java/io/fanloop/FanloopApplication.java
@@ -0,0 +1,11 @@
+package io.fanloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanloopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FanloopApplication.class, args);
+    }
+}
